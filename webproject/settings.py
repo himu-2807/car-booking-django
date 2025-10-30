64205bcd13d59b712b76355e135542c92707a382
Medatiya Himani <himanimedatiya@gmail.com>
2025-10-30 17:20:59 +0530

    Ready for deployment

--- webproject/settings.py
+++ webproject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-auk6@0$aoc2twx&2am#d_eo^gt&!c16&cr6k5y8w_%^bdt8z*7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['HimaniMedatiya.pythonanywhere.com']
 
 
 # Application definition
